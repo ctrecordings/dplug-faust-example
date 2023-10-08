1f42108f29876b461eb19218f35268fb05c2939b
Ethan P. Reker <ethan.reker@wwinc.com>
2023-10-08 11:13:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Dplug Faust Example
+
+![Screenshot](./screenshot.png)
+
+This is an example plugin using Dplug with a Faust backend.  It is a stereo reverb plugin using the Freeverb demo from the Faust library.
---
+++ src/main.d
@@ -0,0 +1,56 @@
+/**
+Copyright: Ethan Reker 2023.
+License: MIT
+*/
+module main;
+
+import std.math;
+import std.algorithm;
+
+import dplug.core,
+       dplug.dsp,
+       dplug.client;
+
+import gui;
+
+import freeverb;
+
+mixin(pluginEntryPoints!ExampleClient);
+
+enum : int
+{
+    paramDamp,
+    paramRoomSize,
+    paramStereoSpread,
+    paramWet
+}
+
+
+final class ExampleClient : FaustClient
+{
+public:
+nothrow:
+@nogc:
+
+    this()
+    {
+        super();
+    }
+
+    override void processAudio(const(float*)[] inputs, float*[]outputs, int frames, TimeInfo info)
+    {
+        assert(frames <= 512);
+        super.processAudio(inputs, outputs, frames, info);
+
+        if (ExampleGUI gui = cast(ExampleGUI) graphicsAcquire())
+        {
+            graphicsRelease();
+        }
+    }
+
+    override IGraphics createGraphics()
+    {
+        return mallocNew!ExampleGUI(this);
+    }
+}
+
